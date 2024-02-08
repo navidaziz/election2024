d3db30e5e55bd948306ea03b6f18ec06aa591d1b
navidaziz98 <navidaziz98@gmail.com>
2024-02-08 21:20:17 +0500

    some changes

--- application/views/admin/polling_stations/election_dashboard.php
+++ application/views/admin/polling_stations/election_dashboard.php
@@ -210,9 +210,7 @@ $candidates = $this->db->query($query)->result();
 
                                 </div>
 
-                                <p style="text-align: center;">
-                                    Design and Developed By <strong> <i>Navid Aziz</i></strong> # 0324 4424424
-                                </p>
+
                             </div>
                         </div>
 
